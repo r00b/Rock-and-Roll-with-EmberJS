4395ff56071a1a0e1793b9051cf003ddf24672c9
Robert Steilberg <rsteilberg@gmail.com>
2017-05-25 11:19:49 -0400

    implement detail editing

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.modelFor('bands.band');
-  },
-
   actions: {
+    willTransition: function(transition) {
+      var controller = this.get('controller'),
+      leave;
+      if (controller.get('isEditing')) {
+        leave = window.confirm("You have unsaved changes. Are you sure you want to leave?");
+        if (leave) {
+          controller.set('isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    },
+    
     didTransition: function() {
       var band = this.modelFor('bands.band');
       document.title = `${band.get('name')} details - Rock & Roll`;
