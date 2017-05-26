d180fb821c263d5043d39ebfebda554bfd86fb57
Robert Steilberg <rsteilberg@gmail.com>
2017-05-26 10:27:36 -0400

    implement song sorting

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   songCreationStarted: false,
 
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedSongs: Ember.computed.sort('model.songs','sortProperties'),
+
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
     return this.get('songCreationStarted') ||
     this.get('model.songs.length');
@@ -13,6 +25,10 @@ export default Ember.Controller.extend({
   }),
 
   actions: {
+    setSorting: function(option) {
+      this.set('sortBy', option);
+    },
+
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
     },
